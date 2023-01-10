704694bbe4f31098e9f1aeb4d3efd1d95d67dcd6
Zakiya-Abass <zakiyaabass@icloud.com>
2023-01-10 23:18:30 +0300

    upperCase

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+
+const currentUser = "Grace Hopper";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+//const shortGreeting = "Welcome, ${currentUser[0]}!";
+
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
